20a751ecd02817fa948a14496cf867ec007a4dcd
ILUNGA_KAYEMBE_GLAN <23ik069si@esisalama.org>
2026-02-02 12:39:25 +0100

    seance 4, add data

--- index.php
+++ index.php
@@ -3,6 +3,21 @@ $header = "Bienvenue sur L'shi Promo";
 ?>
 <?php require 'portions/header.php'; ?>
 
-<h1 class = "text-2xl font-bold text-white">Bienvenue sur L'shi Promo</h1>
+<h1 class = "text-2xl font-bold text-white">Profiter de nos meuilleurs offres </h1>
+
+<?php require 'models/promotions-data.php'; ?>
+
+<?php foreach($promotions as $magasin => $produits) : ?>
+    <div>
+        <h1 class = "text-white"><?php echo $magasin; ?></h1>
+        <?php foreach($produits as $produit => $detail) : ?>
+            <p class="text-white">Nom de l'article : <?php echo $produit; ?></p>
+            <p class="text-white">prix initial : <?php echo $detail['prix-initial']; ?> <?php echo $detail['devise']; ?></p>
+            <p class="text-white">prix promo : <?php echo $detail['prix-promo']; ?> <?php echo $detail['devise']; ?></p>
+            <p class="text-white">Reduction : - <?php echo round(($detail['prix-initial'] - $detail['prix-promo']) * 100 / $detail['prix-initial']); ?>%</p>
+            <br>
+        <?php endforeach; ?>
+    </div>
+<?php endforeach; ?>
 
 <?php require 'portions/footer.php'; ?>
---
+++ models/promotions-data.php
@@ -0,0 +1,23 @@
+<?php 
+$promotions = [
+    'Top-Market' => [
+        'iPhone 16' => [
+            'prix-initial' => 350,
+            'prix-promo' => 280,
+            'devise' => '$',
+        ],
+        'Farine Twiga' => [
+            'prix-initial' => 53000,
+            'prix-promo' => 42000,
+            'devise' => 'FC',
+        ],
+    ],
+    'Kin Marché' => [
+        'Lait NIDO' => [
+            'prix-initial' => 37000,
+            'prix-promo' => 22000,
+            'devise' => 'FC',
+        ],
+    ],
+]
+?>
